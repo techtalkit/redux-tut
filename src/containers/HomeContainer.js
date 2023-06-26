1383e13ae2342a133df2458d5d03118390593446
techtalkitco <techtalkitco@gmail.com>
2023-06-26 10:01:42 +0530

    Container Modified

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,14 @@
+import {connect} from 'react-redux'
 import Home from "../components/Home.js"
+import {addToCart} from '../Services/Actions/actions.js'
+import {removeToCart} from '../Services/Actions/actions.js'
 
-export default Home;
+const mapStateToProps=state=>({
+
+})
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler: data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
+
